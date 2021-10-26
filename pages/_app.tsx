bdfcbebb72250a15bd50dd5cfaa13792ed76195b
bobby ross <bobby.dev@pm.me>
2021-10-26 10:17:33 +0100

    feat: update app page to use theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from 'next-themes';
+import type { AppProps } from 'next/app';
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+    return (
+        <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
+
 export default MyApp
