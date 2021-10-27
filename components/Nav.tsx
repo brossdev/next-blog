7ec8f0e343f36424433f69cea9b93c7a9d43a905
bobby ross <bobby.dev@pm.me>
2021-10-27 12:03:22 +0100

    feat: add first draft about page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,19 +1,19 @@
 import Link from 'next/link';
 
 const Nav = () => {
-  return (
-    <nav>
-      <Link href="/">
-        <a className="text-gray-900 dark:text-white pr-6 py-4">Home</a>
-      </Link>
-      <Link href="/blog">
-        <a className="text-gray-900 dark:text-white px-6 py-4">Blog</a>
-      </Link>
-      <Link href="/about">
-        <a className="text-gray-900 dark:text-white px-6 py-4">About</a>
-      </Link>
-    </nav>
-  );
+    return (
+        <nav>
+            <Link href="/">
+                <a className="text-gray-900 dark:text-white pr-6 py-4">Home</a>
+            </Link>
+            <Link href="/about">
+                <a className="text-gray-900 dark:text-white px-6 py-4">About</a>
+            </Link>
+            <Link href="/blog">
+                <a className="text-gray-900 dark:text-white px-6 py-4">Blog</a>
+            </Link>
+        </nav>
+    );
 };
 
 export default Nav;
--- pages/about.tsx
+++ pages/about.tsx
@@ -9,8 +9,28 @@ export const About = (): JSX.Element => {
         publishedDate: "",
       }}
     >
-      <h1>About Page</h1>
-      <p>Welcome to the about page</p>
+      <h1>About Me</h1>
+      <section>
+        <p>
+          Welcome! I’m Bobby, intentionally or accidentally you have arrived at
+          my personal Blog! I’m a software engineer from Glasgow, Scotland, I
+          worked in finance for around 14 years before I quit my job and joined
+          a coding bootcamp to learn how to code in September 2016. I’ve worked
+          in various roles in the tech industry from a Junior Developer in a
+          Digital Agency to a Tech Lead in a SASS startup.
+        </p>
+        <p>
+          Learning to code changed my life, like any career there are some
+          challenges in Tech, but I truly love what I do, I find being able to
+          contribute and create products that can really help people extremely
+          rewarding. I’m a little obsessed with learning new technologies and
+          building fun projects, I’m continually investigating options for
+          improving my developer tools and experience. Not only that, but I’m
+          probably one of the very people on the planet who went from VS Code to
+          Vim as my main editor, I love being able to navigate everything
+          through the keyboard.
+        </p>
+      </section>
     </Layout>
   );
 };
